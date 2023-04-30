c78a155248114ea4760a5d4c61d5b12ce78446d5
aselka27 <asjumalieva@gmail.com>
2023-04-30 13:47:10 -0500

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by саргашкаева on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
